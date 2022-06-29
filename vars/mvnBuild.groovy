ea0ed25b5aedc1e7753a9e6cf300d8366fd5a0bb
Raghupathi <raghupathiguduri@gmail.com>
2022-06-29 15:28:43 +0530

    Update mvnBuild.groovy

--- vars/mvnBuild.groovy
+++ vars/mvnBuild.groovy
@@ -7,7 +7,7 @@ def call(mode = "") {
         artifact_name = sh(returnStdout: true, script: "echo $JOB_NAME").split('/')[-1].trim().toLowerCase()
         steps.sh "mkdir ${artifact_name}"
         steps.sh "cp target/*.jar ${artifact_name}"
-        steps.sh "zip -j ${artifact_name}.zip ${artifact_name}"
+        steps.sh "zip -j ${artifact_name}.zip ${artifact_name}/*.jar"
     }
     else
     {
