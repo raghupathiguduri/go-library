bf787e02a710fe81156c2dcbbb5a906fb258b5f0
Raghupathi <raghupathiguduri@gmail.com>
2022-06-29 14:54:39 +0530

    Create mvnBuild.groovy

---
+++ vars/mvnBuild.groovy
@@ -0,0 +1,16 @@
+def call(mode = "") {
+    if(!params.BRANCH) {
+        error "Branch parameter is missing"
+    }
+    if(fileExists("pom.xml")) {
+        steps.sh "mvn clean install -DskipTests"
+        artifact_name = sh(returnStdout: true, script: "echo $JOB_NAME").split('/')[-1].trim().toLowerCase()
+        steps.sh "mkdir ${artifact_name}"
+        steps.sh "cp target/*.jar ${artifact_name}"
+        steps.sh "zip -j ${artifact_name}.zip ${artifact_name}"
+    }
+    else
+    {
+        error "There is no pom.xml file in the project"
+    }
+}
