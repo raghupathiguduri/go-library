39bc709390bcb4ec515fbd31f583373596ac0626
Raghupathi <raghupathiguduri@gmail.com>
2022-07-26 12:01:56 +0530

    Update Builddotnet.groovy

--- vars/Builddotnet.groovy
+++ vars/Builddotnet.groovy
@@ -5,8 +5,8 @@ def call(String dotnetcmd)
         error "Branch parameter is missing"
     }
 	try 
-		export PATH=/usr/local/share/dotnet:$PATH
     {	
+	        sh "export PATH=/usr/local/share/dotnet:$PATH"
 		sh "dotnet ${dotnetcmd}"
     }
     catch (Exception ex) 
