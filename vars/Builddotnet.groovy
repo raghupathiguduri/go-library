3b5c6a26eb70536095e04971831580d70bd3d67b
Raghupathi <raghupathiguduri@gmail.com>
2022-07-26 12:44:09 +0530

    Update Builddotnet.groovy

--- vars/Builddotnet.groovy
+++ vars/Builddotnet.groovy
@@ -1,16 +1,22 @@
-def call(String dotnetcmd) 
-{
-    if(!params.BRANCH) 
-    {
-        error "Branch parameter is missing"
-    }
-	try 
-    {	
-	        sh "export PATH=/usr/local/share/dotnet:$PATH"
-		sh "dotnet ${dotnetcmd}"
-    }
-    catch (Exception ex) 
-    {
-        throw ex
-    }
+def call(String dotnetcmd) {
+	if(!params.BRANCH) {
+		error "Branch parameter is missing"
+	}
+	def ApprunTests = utils.loadPipelineProps("runTests")
+	def Appartifact_name = utils.loadPipelineProps("appName")
+	def AppVersion = utils.loadPipelineProps("version")
+	def AppPublish = utils.loadpipelineProps("publish")
+	if(AppPublish.equals(true)) {
+	dotnetPublish
+	}
+	steps.sh "mkdir ${Appartifact_name}-${AppVersion}"
+	if(ApprunTests.equals(true)) { 
+	dotnetTest
+	dotnetBuild --outputDirectory ${Appartifact_name}-${AppVersion}	
+	}
+	else {
+	dotnetBuild --outputDirectory ${Appartifact_name}-${AppVersion}
+	}
+	steps.sh "echo zipping artifact"
+	steps.sh "zip -r ${artifact_name}-${app_version}.zip ${Appartifact_name}-${AppVersion}"
 }
