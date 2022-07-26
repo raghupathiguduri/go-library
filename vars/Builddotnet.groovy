5016c97b4f963de46b378948a5054fa1942e7069
Raghupathi <raghupathiguduri@gmail.com>
2022-07-26 13:23:00 +0530

    Update Builddotnet.groovy

--- vars/Builddotnet.groovy
+++ vars/Builddotnet.groovy
@@ -7,15 +7,15 @@ def call(mode = "") {
 	def AppVersion = utils.loadPipelineProps("version")
 	def AppPublish = utils.loadPipelineProps("publish")
 	if(AppPublish != false ) {
-	sh "dotnetPublish"
+	sh "dotnet publish"
 	}
 	steps.sh "mkdir ${Appartifact_name}-${AppVersion}"
 	if(ApprunTests != false ) { 
-	sh "dotnetTest"
-	sh "dotnetBuild --outputDirectory ${Appartifact_name}-${AppVersion}"
+	sh "dotnet test"
+	sh "dotnet build --output ${Appartifact_name}-${AppVersion}"
 	}
 	else {
-	sh"dotnetBuild --outputDirectory ${Appartifact_name}-${AppVersion}"
+	sh "dotnet build --output ${Appartifact_name}-${AppVersion}"
 	}
 	steps.sh "echo zipping artifact"
 	steps.sh "zip -r '${artifact_name}-${app_version}'.zip '${Appartifact_name}-${AppVersion}'"
