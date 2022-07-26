bbb237f8c69a8bbc0ff000d64c641665f84d9e71
Raghupathi <raghupathiguduri@gmail.com>
2022-07-26 13:07:44 +0530

    Update Builddotnet.groovy

--- vars/Builddotnet.groovy
+++ vars/Builddotnet.groovy
@@ -6,16 +6,16 @@ def call(mode = "") {
 	def Appartifact_name = utils.loadPipelineProps("appName")
 	def AppVersion = utils.loadPipelineProps("version")
 	def AppPublish = utils.loadPipelineProps("publish")
-	if(AppPublish.equals(true)) {
-	steps.sh "dotnetPublish"
+	if(AppPublish != false ) {
+	sh "dotnetPublish"
 	}
 	steps.sh "mkdir ${Appartifact_name}-${AppVersion}"
-	if(ApprunTests.equals(true)) { 
-	steps.sh "dotnetTest"
-	steps.sh "dotnetBuild --outputDirectory ${Appartifact_name}-${AppVersion}"
+	if(ApprunTests != false ) { 
+	sh "dotnetTest"
+	sh "dotnetBuild --outputDirectory ${Appartifact_name}-${AppVersion}"
 	}
 	else {
-	steps.sh"dotnetBuild --outputDirectory ${Appartifact_name}-${AppVersion}"
+	sh"dotnetBuild --outputDirectory ${Appartifact_name}-${AppVersion}"
 	}
 	steps.sh "echo zipping artifact"
 	steps.sh "zip -r '${artifact_name}-${app_version}'.zip '${Appartifact_name}-${AppVersion}'"
